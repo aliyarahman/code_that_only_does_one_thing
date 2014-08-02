043e5d634254cb7e18d85e8d818f14e4ee0fe36e
aliyarahman <aliya.rahman@yahoo.com>
2014-08-01 20:51:19 -0400

    Email to csv finds datetime

--- turn_an_email_text_archive_into_a_sorted_csv/email_to_csv.py
+++ turn_an_email_text_archive_into_a_sorted_csv/email_to_csv.py
@@ -17,16 +17,28 @@ for index, row in enumerate(content):
 # Assemble the csv by writing id, start_line, username, datetime, subject, full_username
 with open(filename+'_sorted'+'.csv', 'wb') as csvfile:
 	archivewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	archivewriter.writerow(["message_number", "starting_line", "ending_line", "message_content"])
+	archivewriter.writerow(["message_number", "starting_line", "ending_line", "username", "datetime","message_content"])
 	for index, start_line in enumerate(start_indexes):
-		username_is_in = content[start_line]
-		full_name_is_in = content[start_line+1]
-		datetime_is_in = content[start_line+2]
+		# email_is_in = content[start_line]
+		### Find the username
+		try:
+			usernameline = ((content[start_line+1]).split("("))
+			username = (usernameline[1].split(")")[0])
+		except:
+			username = "Unknown user on line", start_line
+		# Find the date and time
+		try:
+			datetime = (content[start_line+2].split("Date: ")[1]).strip('\n')
+		except:
+			datetime = "Unknown datetime on line", start_line
 		timezone_is_in = content[start_line+2]
 		subject_is_in = content[start_line+3]
 		try:
 			end_line = start_indexes[index+1]-1
 		except:
 			end_line = len(content)
-		message_content = content[start_line+4:end_line]
-		archivewriter.writerow([index, start_line, end_line, message_content])
+		# Find the message content (too long right now)
+		#####Uncomment###message_content = content[start_line+4:end_line]
+		message_content = "Placeholder_content"
+		# Write the csv file
+		archivewriter.writerow([index, start_line, end_line, username, datetime, message_content])
