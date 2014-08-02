71593db5961c20a70a09965348528886df8d151f
aliyarahman <aliya.rahman@yahoo.com>
2014-08-01 20:10:35 -0400

    email_to_csv outputs csv with message_id and starting line

--- turn_an_email_text_archive_into_a_sorted_csv/email_to_csv.py
+++ turn_an_email_text_archive_into_a_sorted_csv/email_to_csv.py
@@ -1,14 +1,19 @@
+import csv
+
 #Open the file
-with open("commotion-discuss.txt", "r") as archive_file:
+filename = "commotion-discuss.txt"
+with open(filename, "r") as archive_file:
 	content = archive_file.readlines()
 
 #  Find the indexes for all of the messages' starting lines
 counter = 0
 start_indexes = []
-for index, row in enumerate(content):
-	if "From " in row[0:5]:
-		start_indexes.append(index)
-		counter +=1
 
-print counter
-print start_indexes
+
+with open(filename+'_sorted'+'.csv', 'wb') as csvfile:
+	archivewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+	archivewriter.writerow(["message_number", "starting_line"])
+	for index, row in enumerate(content):
+		if "From " in row[0:5]:
+			counter +=1
+			archivewriter.writerow([counter, index])
